2dc1f5fe61401db89e1ff0f2d8ec5e66f42ec77c
Bastianwx <sebastiancera2005@gmail.com>
2025-07-07 17:15:51 -0500

    nueva ruta

--- frontend/lib/routes/routes.dart
+++ frontend/lib/routes/routes.dart
@@ -1,18 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:design_alma/app/main_scaffold.dart';
 import 'package:design_alma/feature/login/screens/Forgot_Password_Screen.dart';
 import 'package:design_alma/feature/login/screens/Register_Screen.dart';
 import 'package:design_alma/feature/login/screens/login_screen.dart';
-
-import 'package:flutter/material.dart';
 
 class AppRoute {
   static const String home = '/login';
   static const String login = '/login';
   static const String forgotPassword = '/forgot-password';
   static const String register = '/register';
+  static const String perfil = '/perfil';
 
   final Map<String, WidgetBuilder> routes = {
     login: (context) => const LoginScreen(),
     forgotPassword: (context) => const ForgotPasswordScreen(),
     register: (context) => const RegisterScreen(),
+    perfil: (context) => MainScaffold(initialIndex: 4),
   };
 }
