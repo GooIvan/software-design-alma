df09a9dd4e1238ef34a8daf1d6e185c6c27af0f7
BrianOrtegaa <brianorte8@gmail.com>
2025-07-07 13:37:34 -0500

    models

--- backend/app/models/cart.rb
+++ backend/app/models/cart.rb
@@ -1,5 +1,10 @@
 # app/models/cart.rb
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
+
+  def total_price
+    cart_items.sum { |item| item.product.price * item.quantity }
+  end
+
 end
 
