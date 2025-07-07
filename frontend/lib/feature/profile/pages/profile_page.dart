f8985c7a8c54f19cc5348e50a62755ad31af48f6
Bastianwx <sebastiancera2005@gmail.com>
2025-07-07 17:34:27 -0500

    cerrar sesion

--- frontend/lib/feature/profile/pages/profile_page.dart
+++ frontend/lib/feature/profile/pages/profile_page.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '../bloc/perfil_bloc.dart';
 import '../bloc/perfil_event.dart';
 import '../bloc/perfil_state.dart';
 import '../views/perfil_loading_view.dart';
 import '../views/perfil_error_view.dart';
 import '../views/perfil_view.dart';
+import '../../../routes/routes.dart';
 
 class PerfilPage extends StatelessWidget {
   const PerfilPage({super.key});
+
+  Future<void> _cerrarSesion(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('isLoggedIn');
+    await prefs.remove('userEmail');
+
+    // Navegar al login eliminando historial
+    Navigator.pushNamedAndRemoveUntil(context, AppRoute.login, (route) => false);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +29,8 @@ class PerfilPage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Mi Perfil'),
-          backgroundColor: Color.fromARGB(255, 70, 140, 247),
+          backgroundColor: const Color.fromARGB(255, 70, 140, 247),
+          // Eliminamos el botón de cerrar sesión del AppBar
         ),
         body: BlocBuilder<PerfilBloc, PerfilState>(
           builder: (context, state) {
@@ -31,7 +44,10 @@ class PerfilPage extends StatelessWidget {
                 },
               );
             } else if (state is PerfilLoaded) {
-              return PerfilView(nombreUsuario: state.nombreUsuario);
+              return PerfilView(
+                nombreUsuario: state.nombreUsuario,
+                onCerrarSesion: () => _cerrarSesion(context),
+              );
             }
             return const SizedBox.shrink();
           },
