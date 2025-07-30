0cc314acc08a9b0335affa7a8da0ba85be2eb9db
SaidColey456 <168228442+SaidColey456@users.noreply.github.com>
2025-07-30 15:16:36 -0500

    adds test users

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -124,7 +124,7 @@ end
 
 # 🧪 Crear usuarios de prueba
 test_users = [
-  { name: "Juan", last_name: "Pérez", email: "juan@example.com" },
+  { name: "Juan", last_name: "Pérez", email: "juan@example.com", password: "test1234", password_confirmation: "test1234", city: "Ciudad", address: "Dirección 123", role: "customer"},
   { name: "Ana", last_name: "Gómez", email: "ana@example.com" },
   { name: "Luis", last_name: "Torres", email: "luis@example.com" }
 ]
