c5c29f1cc86ed25f44469f13e2421e05d95bd9f5
Bastianwx <sebastiancera2005@gmail.com>
2025-08-04 17:49:43 -0500

    perfil bloc

--- frontend/lib/feature/profile/bloc/perfil_bloc.dart
+++ frontend/lib/feature/profile/bloc/perfil_bloc.dart
@@ -1,18 +1,26 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'perfil_event.dart';
 import 'perfil_state.dart';
+import '../../login/services/auth_service.dart';
 
 class PerfilBloc extends Bloc<PerfilEvent, PerfilState> {
+  final AuthService _authService = AuthService();
+
   PerfilBloc() : super(PerfilInitial()) {
     on<CargarPerfil>((event, emit) async {
       emit(PerfilLoading());
 
       try {
-        await Future.delayed(const Duration(seconds: 1));
-        const nombre = 'Alma';
-        emit(PerfilLoaded(nombreUsuario: nombre));
-      } catch (_) {
-        emit(PerfilError(mensaje: 'No se pudo cargar el perfil'));
+        final userData = await _authService.getUserData();
+        final nombre = userData['name'];
+
+        if (nombre != null && nombre.isNotEmpty) {
+          emit(PerfilLoaded(nombreUsuario: nombre));
+        } else {
+          emit(PerfilError(mensaje: 'No se pudo cargar el nombre de usuario.'));
+        }
+      } catch (e) {
+        emit(PerfilError(mensaje: 'Error al cargar el perfil.'));
       }
     });
   }
