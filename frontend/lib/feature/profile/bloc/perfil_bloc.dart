bbcba9a8dc88626a6d1e12730c26d6b3ce7222fa
Bastianwx <sebastiancera2005@gmail.com>
2025-08-06 16:54:18 -0500

    new perfil_bloc and main

--- frontend/lib/feature/profile/bloc/perfil_bloc.dart
+++ frontend/lib/feature/profile/bloc/perfil_bloc.dart
@@ -1,23 +1,33 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import 'perfil_event.dart';
 import 'perfil_state.dart';
 import '../../login/services/auth_service.dart';
 
 class PerfilBloc extends Bloc<PerfilEvent, PerfilState> {
   final AuthService _authService = AuthService();
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
   PerfilBloc() : super(PerfilInitial()) {
     on<CargarPerfil>((event, emit) async {
       emit(PerfilLoading());
 
       try {
-        final userData = await _authService.getUserData();
-        final nombre = userData['name'];
+        final token = await _storage.read(key: 'authToken');
 
-        if (nombre != null && nombre.isNotEmpty) {
-          emit(PerfilLoaded(nombreUsuario: nombre));
+        if (token == null) {
+        
+          emit(PerfilLoaded(nombreUsuario: ''));
         } else {
-          emit(PerfilError(mensaje: 'No se pudo cargar el nombre de usuario.'));
+          final userData = await _authService.getUserData();
+          final nombre = userData['name'];
+
+          if (nombre != null && nombre.isNotEmpty) {
+            emit(PerfilLoaded(nombreUsuario: nombre));
+          } else {
+            emit(PerfilError(mensaje: 'No se pudo cargar el nombre de usuario.'));
+          }
         }
       } catch (e) {
         emit(PerfilError(mensaje: 'Error al cargar el perfil.'));
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,23 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:design_alma/routes/routes.dart';
 import 'package:design_alma/screens/logo_intro.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final storage = FlutterSecureStorage();
-  final token = await storage.read(key: 'authToken');
-
-
-  final bool isLoggedIn = token != null;
-
-  runApp(MyApp(isLoggedIn: isLoggedIn));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final bool isLoggedIn;
-
-  const MyApp({super.key, required this.isLoggedIn});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -30,9 +21,11 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       title: 'DesignAlma App',
-
-      initialRoute: AppRoute.main,
-      routes: AppRoute.routes,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LogoIntro(),
+        ...AppRoute.routes,
+      },
     );
   }
 }
