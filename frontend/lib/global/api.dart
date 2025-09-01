9f475e8d3e63e7ab21c3abc533e8252efd3e9dbe
Iván Madera <maderamartinezivan@gmail.com>
2025-09-01 12:17:29 -0500

    .

--- frontend/lib/global/api.dart
+++ frontend/lib/global/api.dart
@@ -1,7 +1,7 @@
 class Api {
   // Using ngrok for local development
-  // static const String baseUrl = 'https://designalma.loca.lt';
+  static const String baseUrl = 'https://designalma.loca.lt';
 
   // Using localhost for local development
-  static const String baseUrl = 'https://7e565e35ea3c.ngrok-free.app';
+  // static const String baseUrl = 'http://localhost:3000';
 }
